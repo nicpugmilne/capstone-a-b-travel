b33492792fac80a69daa78802890d81cb70a5178
Nicola Milne <nicolalouisemilne@gmail.com>
2022-08-15 17:54:49 -0600

    removed requirement for duration on Itinerary module

--- app/controllers/itinerary_modules_controller.rb
+++ app/controllers/itinerary_modules_controller.rb
@@ -1,5 +1,6 @@
 class ItineraryModulesController < ApplicationController
     def create
+        byebug
         itinerary_module = ItineraryModule.create!(params.permit(:module_type_id, :itinerary_id, :name, :start_datetime, :end_datetime, :duration, :cost, :notes))
         render json: itinerary_module, status: :created
     end
