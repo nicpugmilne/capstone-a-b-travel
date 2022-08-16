75dc27069cbe9fa0d58d470b8199a14207e5f6a0
Nicola Milne <nicolalouisemilne@gmail.com>
2022-08-16 14:46:49 -0600

    now dynamically updating itinerary summary when new items are added

---
+++ client/src/components/itinerary/ItinerarySummary.js
@@ -0,0 +1,18 @@
+import { Text, Stack } from "@chakra-ui/react";
+
+export default function ItinerarySummary({ itinerary }) {
+  return (
+    <Stack align={"center"} justify={"center"} mb="10">
+      <Text fontSize={"lg"}>Summary:</Text>
+      <Text fontSize={"sm"} fontWeight={500}>
+        Total Cost: ${itinerary.total_cost}
+      </Text>
+      <Text fontSize={"sm"} fontWeight={500}>
+        Dates: {itinerary.itinerary_start_date} - {itinerary.itinerary_end_date}
+      </Text>
+      <Text fontSize={"sm"} fontWeight={500}>
+        Travel time: {(itinerary.travel_time / 60).toPrecision(3)} hrs
+      </Text>
+    </Stack>
+  );
+}
