9eb44b630de78892477856bf66aee938ff780d3e
Nicola Milne <nicolalouisemilne@gmail.com>
2022-08-16 17:37:06 -0600

    delete trip from db works

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,6 +1,6 @@
 class Trip < ApplicationRecord
   belongs_to :user
-  has_many :itineraries
+  has_many :itineraries, dependent: :destroy
   has_many :itinerary_modules, through: :itineraries
   validates :user_id, presence: true
   validates :name, presence: true
